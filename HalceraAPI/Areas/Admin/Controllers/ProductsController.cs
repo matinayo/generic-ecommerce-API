c060ef5e7f7e2a25ea0b5767f735aa6773695de0
Abdul-matin <madeayo04@gmail.com>
2023-04-22 20:55:32 +0000

    base route definition

---
+++ HalceraAPI/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalceraAPI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("api/[area]/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ HalceraAPI/Areas/Customer/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalceraAPI.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Route("api/[area]/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
