6492b1cfb0c2035c2db36a407d52773327968488
Abdul-matin <madeayo04@gmail.com>
2023-04-25 23:45:30 +0000

    define product endpoint

--- HalceraAPI/Areas/Customer/Controllers/ProductsController.cs
+++ HalceraAPI/Areas/Customer/Controllers/ProductsController.cs
@@ -15,6 +15,10 @@ namespace HalceraAPI.Areas.Customer.Controllers
             _productOperation = productOperation;
         }
 
+        /// <summary>
+        /// Get list of products
+        /// </summary>
+        /// <returns>List of products</returns>
         [HttpGet]
         [Route("GetAll")]
         [ProducesResponseType(typeof(IEnumerable<Product>), 200)]
@@ -25,6 +29,29 @@ namespace HalceraAPI.Areas.Customer.Controllers
             {
                 IEnumerable<Product>? listOfProducts = await _productOperation.GetAllProducts();
                 return Ok(listOfProducts);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(Problem(statusCode: StatusCodes.Status400BadRequest, detail: exception.Message));
+            }
+        }
+
+        /// <summary>
+        /// Get product by Id
+        /// </summary>
+        /// <param name="productId">id of product</param>
+        /// <returns>Product details from DB</returns>
+        [HttpGet]
+        [Route("GetProduct/{productId}")]
+        [ProducesResponseType(typeof(Product), 200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<Product?>> GetProduct(int productId)
+        {
+            try
+            {
+                Product? productFromDb = await _productOperation.GetProductById(productId);
+                return Ok(productFromDb);
             }
             catch (Exception exception)
             {
