e2a33348b7e7da24eafb43de527ad9c83d431013
Abdul-matin Adebayo <adebayo@safepay.money>
2023-12-09 20:38:22 +0000

    admin order endpoints

--- HalceraAPI.Services/Contract/IAdminOrderOperation.cs
+++ HalceraAPI.Services/Contract/IAdminOrderOperation.cs
@@ -5,7 +5,7 @@ namespace HalceraAPI.Services.Contract
 {
     public interface IAdminOrderOperation
     {
-        Task<OrderResponse> GetOrderById(string orderId);
+        Task<OrderResponse> GetOrderByIdAsync(string orderId);
         Task<IEnumerable<OrderResponse>> GetOrdersAsync(OrderStatus? orderStatus);
         Task<UpdateOrderStatusResponse> UpdateOrderStatusAsync(string orderId, OrderStatus orderStatus);
     }
--- HalceraAPI/Areas/Admin/Controllers/OrdersController.cs
+++ HalceraAPI/Areas/Admin/Controllers/OrdersController.cs
@@ -1,14 +1,8 @@
-﻿using AutoMapper.Features;
-using HalceraAPI.Models;
-using HalceraAPI.Models.Enums;
+﻿using HalceraAPI.Models.Enums;
 using HalceraAPI.Models.Requests.OrderHeader;
-using HalceraAPI.Models.Requests.Product;
 using HalceraAPI.Services.Contract;
-using HalceraAPI.Services.Operations;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 
 namespace HalceraAPI.Areas.Admin.Controllers
 {
@@ -26,6 +20,8 @@ namespace HalceraAPI.Areas.Admin.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>), 200)]
+        [ProducesResponseType(404)]
         public async Task<ActionResult<IEnumerable<OrderResponse>>> GetOrdersAsync(OrderStatus? orderStatus)
         {
             try
@@ -33,6 +29,46 @@ namespace HalceraAPI.Areas.Admin.Controllers
                 IEnumerable<OrderResponse>? listOfOrders = await _orderOperation.GetOrdersAsync(orderStatus);
 
                 return Ok(listOfOrders);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(
+                    Problem(
+                        statusCode: StatusCodes.Status400BadRequest,
+                        detail: exception?.InnerException?.Message ?? exception?.Message));
+            }
+        }
+
+        [HttpGet("{orderId}")]
+        [ProducesResponseType(typeof(OrderResponse), 200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<OrderResponse>> GetOrderByIdAsync(string orderId)
+        {
+            try
+            {
+                OrderResponse orderDetails = await _orderOperation.GetOrderByIdAsync(orderId);
+
+                return Ok(orderDetails);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(
+                    Problem(
+                        statusCode: StatusCodes.Status400BadRequest,
+                        detail: exception?.InnerException?.Message ?? exception?.Message));
+            }
+        }
+
+        [HttpPut("{orderId}/{orderStatus}")]
+        [ProducesResponseType(typeof(UpdateOrderStatusResponse), 200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<UpdateOrderStatusResponse>> UpdateOrderStatusAsync(string orderId, OrderStatus orderStatus)
+        {
+            try
+            {
+                UpdateOrderStatusResponse updateOrderDetails = await _orderOperation.UpdateOrderStatusAsync(orderId, orderStatus);
+
+                return Ok(updateOrderDetails);
             }
             catch (Exception exception)
             {
