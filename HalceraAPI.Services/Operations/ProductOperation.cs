732e43b2186a8f9466a990ea2ef8d9633bd287f9
Abdul-matin Adebayo <adebayo@safepay.money>
2023-12-13 23:16:57 +0000

    product pagination

--- HalceraAPI.Services/Operations/ProductOperation.cs
+++ HalceraAPI.Services/Operations/ProductOperation.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
+using AutoMapper.Features;
 using HalceraAPI.Common.Utilities;
 using HalceraAPI.DataAccess.Contract;
 using HalceraAPI.Models;
 using HalceraAPI.Models.Requests.APIResponse;
 using HalceraAPI.Models.Requests.Product;
 using HalceraAPI.Services.Contract;
+using System.Linq.Expressions;
 
 namespace HalceraAPI.Services.Operations
 {
@@ -65,9 +67,8 @@ namespace HalceraAPI.Services.Operations
         {
             try
             {
-                Product? productDetails = await _unitOfWork.Product.GetFirstOrDefault(product => product.Id == productId);
-                if (productDetails == null)
-                    throw new Exception("Product not found");
+                Product? productDetails = await _unitOfWork.Product.GetFirstOrDefault(product => product.Id == productId) 
+                    ?? throw new Exception("Product not found");
 
                 // delete product media
                 await _mediaOperation.DeleteMediaCollection(null, productId);
@@ -78,6 +79,7 @@ namespace HalceraAPI.Services.Operations
 
                 _unitOfWork.Product.Remove(productDetails);
                 await _unitOfWork.SaveAsync();
+
                 return true;
             }
             catch (Exception)
@@ -90,47 +92,47 @@ namespace HalceraAPI.Services.Operations
         {
             try
             {
-                IEnumerable<Product>? listOfProducts = Enumerable.Empty<Product>();
+                Expression<Func<Product, bool>>? filterExpression = null;
+
                 if (active.HasValue && featured.HasValue && categoryId.HasValue)
                 {
-                    listOfProducts = await GetActiveAndFeaturedProductsByCategoryId(active, featured, categoryId.Value, listOfProducts, page);
+                    filterExpression = GetActiveAndFeaturedProductsByCategoryId(active, featured, categoryId.Value);
                 }
                 else if (active.HasValue && categoryId.HasValue)
                 {
-                    listOfProducts = await GetActiveProductsByCategoryId(active, categoryId.Value, listOfProducts, page);
+                    filterExpression = GetActiveProductsByCategoryId(active, categoryId.Value);
                 }
                 else if (featured.HasValue && categoryId.HasValue)
                 {
-                    listOfProducts = await GetFeaturedProductsByCategoryId(featured, categoryId.Value, listOfProducts, page);
+                    filterExpression = GetFeaturedProductsByCategoryId(featured, categoryId.Value);
                 }
                 else if (active.HasValue && featured.HasValue)
                 {
-                    listOfProducts = await GetActiveAndFeaturedProducts(active, featured, listOfProducts, page);
+                    filterExpression = GetActiveAndFeaturedProducts(active, featured);
                 }
                 else if (categoryId.HasValue)
                 {
-                    listOfProducts = await GetProductsByCategoryId(categoryId.Value, listOfProducts, page);
+                    filterExpression = GetProductsByCategoryId(categoryId.Value);
                 }
                 else if (active.HasValue)
                 {
-                    listOfProducts = await GetActiveProducts(active, listOfProducts, page);
+                    filterExpression = GetActiveProducts(active);
                 }
                 else if (featured.HasValue)
                 {
-                    listOfProducts = await GetFeaturedProducts(featured, listOfProducts, page);
-                }
-                else
-                {
-                    listOfProducts = await GetAllProducts(listOfProducts, page);
+                    filterExpression = GetFeaturedProducts(featured);
                 }
 
-                IEnumerable<ProductResponse>? listOfProductResponse = new List<ProductResponse>();
-                if (listOfProducts is not null && listOfProducts.Any())
-                {
-                    _mapper.Map(listOfProducts, listOfProductResponse);
-                }
+                int totalItems = await _unitOfWork.Product.CountAsync(filterExpression);
+                IEnumerable<ProductResponse>? listOfProductResponse = await _unitOfWork.Product.GetAll<ProductResponse>(
+                 filter: filterExpression,
+                 includeProperties: $"{nameof(Product.Categories)},{nameof(Product.MediaCollection)},{nameof(Product.Prices)}",
+                 skip: ((page ?? 1) - 1) * Pagination.DefaultItemsPerPage,
+                 take: Pagination.DefaultItemsPerPage);
 
-                return new APIResponse<IEnumerable<ProductResponse>>(listOfProductResponse);
+                var meta = new Meta(totalItems, Pagination.DefaultItemsPerPage, page ?? 1);
+
+                return new APIResponse<IEnumerable<ProductResponse>>(listOfProductResponse, meta);
             }
             catch (Exception)
             {
@@ -197,152 +199,27 @@ namespace HalceraAPI.Services.Operations
             }
         }
 
-        private async Task<IEnumerable<Product>> GetAllProducts(IEnumerable<Product>? listOfProducts, int? page)
-        {
-            listOfProducts = await _unitOfWork.Product.GetAll(
-                                    includeProperties: $"{nameof(Product.Categories)},{nameof(Product.MediaCollection)},{nameof(Product.Prices)}", 
-                                    skip: ((page ?? 1) - 1) * Pagination.DefaultItemsPerPage,
-                                    take: Pagination.DefaultItemsPerPage);
+        private static Expression<Func<Product, bool>> GetFeaturedProducts(bool? featured) => product => product.Featured == featured;
 
-            return listOfProducts;
-        }
+        private static Expression<Func<Product, bool>> GetActiveProducts(bool? active) => product => product.Active == active;
 
-        /// <summary>
-        /// Get products by featured query
-        /// </summary>
-        /// <param name="featured">Featured query</param>
-        /// <param name="listOfProducts">IEnumerable</param>
-        /// <returns>List of filtered products from DB</returns>
-        private async Task<IEnumerable<Product>> GetFeaturedProducts(bool? featured, IEnumerable<Product>? listOfProducts, int? page)
-        {
-            listOfProducts = await _unitOfWork.Product.GetAll(product => product.Featured == featured,
-                                    includeProperties: $"{nameof(Product.Categories)},{nameof(Product.MediaCollection)},{nameof(Product.Prices)}", 
-                                    skip: ((page ?? 1) - 1) * Pagination.DefaultItemsPerPage,
-                                    take: Pagination.DefaultItemsPerPage);
+        private static Expression<Func<Product, bool>> GetActiveAndFeaturedProducts(
+            bool? active, bool? featured) => product => product.Active == active && product.Featured == featured;
 
-            return listOfProducts;
-        }
+        private static Expression<Func<Product, bool>> GetFeaturedProductsByCategoryId(
+            bool? featured,int categoryId) =>
+            product => product.Featured == featured && product.Categories!.Any(category => category.Id == categoryId);
 
-        /// <summary>
-        /// Get products by active query
-        /// </summary>
-        /// <param name="active">Active query</param>
-        /// <param name="listOfProducts">IEnumerable</param>
-        /// <returns>List of filtered products from DB</returns>
-        private async Task<IEnumerable<Product>> GetActiveProducts(bool? active, IEnumerable<Product>? listOfProducts, int? page)
-        {
-            listOfProducts = await _unitOfWork.Product.GetAll(product => product.Active == active,
-                                    includeProperties: $"{nameof(Product.Categories)},{nameof(Product.MediaCollection)},{nameof(Product.Prices)}", 
-                                    skip: ((page ?? 1) - 1) * Pagination.DefaultItemsPerPage,
-                                    take: Pagination.DefaultItemsPerPage);
+        private static Expression<Func<Product, bool>> GetActiveProductsByCategoryId(
+            bool? active, int categoryId) =>
+            product => product.Active == active && product.Categories!.Any(category => category.Id == categoryId);
 
-            return listOfProducts;
-        }
+        private static Expression<Func<Product, bool>> GetActiveAndFeaturedProductsByCategoryId(
+            bool? active, bool? featured, int categoryId) => 
+            product => product.Active == active && product.Featured == featured 
+            && product.Categories!.Any(category => category.Id == categoryId);
 
-        /// <summary>
-        /// Get products by both active and featured query
-        /// </summary>
-        /// <param name="active">Active query</param>
-        /// <param name="featured">Featured query</param>
-        /// <param name="listOfProducts">IEnumerable</param>
-        /// <returns>List of filtered products from DB</returns>
-        private async Task<IEnumerable<Product>> GetActiveAndFeaturedProducts(
-            bool? active,
-            bool? featured, 
-            IEnumerable<Product>? listOfProducts,
-            int? page)
-        {
-            listOfProducts = await _unitOfWork.Product.GetAll(
-                 product => product.Active == active && product.Featured == featured,
-                 includeProperties: $"{nameof(Product.Categories)},{nameof(Product.MediaCollection)},{nameof(Product.Prices)}",
-                 skip: ((page ?? 1) - 1) * Pagination.DefaultItemsPerPage,
-                 take: Pagination.DefaultItemsPerPage);
-
-            return listOfProducts;
-        }
-
-        /// <summary>
-        /// Get Featured products by category Id
-        /// </summary>
-        /// <param name="featured">Featured query</param>
-        /// <param name="categoryId">Category Id</param>
-        /// <param name="listOfProducts">IEnumerable</param>
-        /// <returns>List of filtered products by category</returns>
-        private async Task<IEnumerable<Product>> GetFeaturedProductsByCategoryId(
-            bool? featured,
-            int categoryId,
-            IEnumerable<Product>? listOfProducts,
-            int? page)
-        {
-            listOfProducts = await _unitOfWork.Product.GetAll(
-                                    product => product.Featured == featured
-                                    && product.Categories!.Any(category => category.Id == categoryId),
-                                    includeProperties: $"{nameof(Product.Categories)},{nameof(Product.MediaCollection)},{nameof(Product.Prices)}",
-                                    skip: ((page ?? 1) - 1) * Pagination.DefaultItemsPerPage,
-                                    take: Pagination.DefaultItemsPerPage);
-
-            return listOfProducts;
-        }
-
-        /// <summary>
-        /// Get Active products by category Id
-        /// </summary>
-        /// <param name="active">Active query</param>
-        /// <param name="categoryId">Category Id</param>
-        /// <param name="listOfProducts">IEnumerable</param>
-        /// <returns>List of filtered products by category</returns>
-        private async Task<IEnumerable<Product>> GetActiveProductsByCategoryId(bool? active, int categoryId, IEnumerable<Product>? listOfProducts, int? page)
-        {
-            listOfProducts = await _unitOfWork.Product.GetAll(
-                                    product => product.Active == active
-                                    && product.Categories!.Any(category => category.Id == categoryId),
-                                    includeProperties: $"{nameof(Product.Categories)},{nameof(Product.MediaCollection)},{nameof(Product.Prices)}",
-                                    skip: ((page ?? 1) - 1) * Pagination.DefaultItemsPerPage,
-                                    take: Pagination.DefaultItemsPerPage);
-
-            return listOfProducts;
-        }
-
-        /// <summary>
-        /// Get Active and Featured products by category Id
-        /// </summary>
-        /// <param name="active">Active query</param>
-        /// <param name="featured">Featured query</param>
-        /// <param name="categoryId">Category Id</param>
-        /// <param name="listOfProducts">IEnumerable</param>
-        /// <returns>List of filtered products by category</returns>
-        private async Task<IEnumerable<Product>> GetActiveAndFeaturedProductsByCategoryId(
-            bool? active,
-            bool? featured,
-            int categoryId,
-            IEnumerable<Product>? listOfProducts,
-            int? page)
-        {
-            listOfProducts = await _unitOfWork.Product.GetAll(
-                                    product => product.Active == active && product.Featured == featured
-                                    && product.Categories!.Any(category => category.Id == categoryId),
-                                    includeProperties: $"{nameof(Product.Categories)},{nameof(Product.MediaCollection)},{nameof(Product.Prices)}",
-                                    skip: ((page ?? 1) - 1) * Pagination.DefaultItemsPerPage,
-                                    take: Pagination.DefaultItemsPerPage);
-
-            return listOfProducts;
-        }
-
-        /// <summary>
-        /// Get products by category Id
-        /// </summary>
-        /// <param name="categoryId">Category Id</param>
-        /// <param name="listOfProducts">IEnumerable</param>
-        /// <returns>List of filtered products by category</returns>
-        private async Task<IEnumerable<Product>> GetProductsByCategoryId(int categoryId, IEnumerable<Product>? listOfProducts, int? page)
-        {
-            listOfProducts = await _unitOfWork.Product.GetAll(
-                                    filter: product => product.Categories!.Any(category => category.Id == categoryId),
-                                   includeProperties: $"{nameof(Product.Categories)},{nameof(Product.MediaCollection)},{nameof(Product.Prices)}",
-                                   skip: ((page ?? 1) - 1) * Pagination.DefaultItemsPerPage,
-                                   take: Pagination.DefaultItemsPerPage);
-
-            return listOfProducts;
-        }
+        private static Expression<Func<Product, bool>> GetProductsByCategoryId(int categoryId) =>
+            product => product.Categories!.Any(category => category.Id == categoryId);
     }
 }
